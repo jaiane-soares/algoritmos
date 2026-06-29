09e1abb2feaf735fdcf98fbff8b9479a23b66ba3
Jaiane <jaianesoares700@gmail.com>
2026-06-29 10:01:40 -0300

    feat: feito exercício um

---
+++ 01 - ExercicioUm.cs
@@ -0,0 +1,12 @@
+/*  Crie um programa que receba a largura e a altura de um retângulo (pode fixar os valores em variáveis)
+ e calcule a área (largura $\times$ altura).*/
+
+
+using System;
+
+double altura = 10.4;
+double largura = 3.2;
+
+double area = largura * altura;
+
+Console.WriteLine("A área do retângulo é de " + area);
